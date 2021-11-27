6f45a58f131fd0917206d080c3033df8c6fefd15
Adrienne <95113407+jiigggeee@users.noreply.github.com>
2021-11-26 23:29:00 -0500

    Simple Python List
    With twists.

---
+++ lists.py
@@ -0,0 +1,8 @@
+friends_family = ["Dave", "Wil", "Mom", "Dad", "Grannie"]
+friends_family[2] = "Janice"
+friends_family[3] = "Bill"
+friends_family[4] = "Mary Louise"
+
+print(friends_family[2])
+print(friends_family[3])
+print(friends_family[4])
