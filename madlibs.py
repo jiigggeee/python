57bfb9fa8c9f28f1e433a544d1d3d95fff6d42d0
Adrienne <95113407+jiigggeee@users.noreply.github.com>
2021-11-26 22:24:28 -0500

    Python Madlibs Exercise
    Simple Python Madlibs exercise that can be customized in many ways.

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+color = input("Enter a color, Karen: ")
+plural_noun = input("Enter a plural noun, Chad: ")
+celebrity = input("Name a celebrity, Sharon: ")
+funny_place = input("What is a funny place, Harge: ")
+
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print(celebrity + " is in jail")
+print("Would you like to go to " + funny_place)
