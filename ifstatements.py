844b1196b5372d9e42b30299514c4e2037b8d63f
Adrienne <95113407+jiigggeee@users.noreply.github.com>
2021-11-28 03:05:21 -0500

    If Statements Exercise
    This was ALOT.

---
+++ ifstatements.py
@@ -0,0 +1,11 @@
+is_gay = True
+is_raccoon = False
+
+if is_gay and is_raccoon:
+    print("Hi gay trash panda! Sashay into a deal today!")
+elif is_gay and not(is_raccoon):
+    print("Call your mother! She worries!")
+elif not is_gay and is_raccoon:
+    print("Linda Belcher loves you, Little King Trashmouth")
+else:
+    print("Why are you here, Roberta Trash!?")
