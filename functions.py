03b94bf4190651d8c9ba1dc91c4100cbf8ee6ba2
Adrienne <95113407+jiigggeee@users.noreply.github.com>
2021-11-28 02:07:29 -0500

    Functions Practice
    Practiced functions. Getting back into the swing of things.

---
+++ functions.py
@@ -0,0 +1,7 @@
+def sayhi(name, age, personality):
+    print("Aloha " + name + ", you are " + age + ", and a " + personality)
+
+sayhi("Angela", "77", "revolutionary.")
+sayhi("Dave", "52", "baseball fan.")
+sayhi("Jaboukie", "28", "social media troll.")
+
