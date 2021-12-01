1aee53cda320d0efdf4ac3ba35b1cc56967b5b3d
Adrienne <95113407+jiigggeee@users.noreply.github.com>
2021-11-30 22:16:37 -0500

    Dictionaries "monthConversions" exercise

---
+++ dictionaries.py
@@ -0,0 +1,16 @@
+monthConversions ={
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sept": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+
+}
+print(monthConversions.get("Luv", "Not a valid key."))
