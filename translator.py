ddada67f5b2defbaa460feb3ae9f340787ede7b2
Adrienne <95113407+jiigggeee@users.noreply.github.com>
2021-11-30 22:13:50 -0500

    Python Translator

---
+++ translator.py
@@ -0,0 +1,10 @@
+def translator(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translator(input("Enter a phrase")))
