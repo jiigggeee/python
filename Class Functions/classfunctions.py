29babf62dae81ea4f854722b3cc05e4e257a50de
Adrienne <95113407+jiigggeee@users.noreply.github.com>
2021-12-08 08:45:12 -0500

    Class functions exercise using student data

---
+++ Class Functions/classfunctions.py
@@ -0,0 +1,7 @@
+from student1 import student
+
+student_a = student("Oscar Wilde", "Drama", 2.8)
+student_b = student("Patricia Highsmith", "Queer Studies", 1.6)
+
+print(student_a.on_honor_roll())
+print(student_b.on_honor_roll())
