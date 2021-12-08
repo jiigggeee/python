ae788670cd21e90deb2539b5a784867a92119a7b
Adrienne <95113407+jiigggeee@users.noreply.github.com>
2021-12-08 01:45:44 -0500

    Student objects in Python
    This file imports from the students file

---
+++ Student Classes and Objects/classes.py
@@ -0,0 +1,7 @@
+from student import Student
+
+student1 = Student("Angela Lansbury", "Gender Studies", 3.69, False)
+student2 = Student("Julie Andrews", "Art History", 4.00, False)
+
+print(student1.name)
+print(student2.major)
