4173519e8192773de0400cd8b1e475a400bedbc7
Adrienne <95113407+jiigggeee@users.noreply.github.com>
2022-01-01 03:37:30 -0500

    Stripper Name Generator
    A stripper name generator using Python

---
+++ strippernamegenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Stripper Name Generator!")
+
+pet = input("What was your first pet's name? ")
+street = input("What was the street name you lived on as a child? ")
+
+print("You stripper name is: " + (pet) + " " + (street))
