1dea7b198a9629c2914fdb053a76f89eaefb92a0
jiigggeee <95113407+jiigggeee@users.noreply.github.com>
2023-12-03 17:49:29 -0500

    DnD Character Creator
    DnD character creator using Python. Very elementary yet helpful.

---
+++ DND character creator.py
@@ -0,0 +1,18 @@
+name = ''
+gender = ''
+race = ''
+description = ''
+name = input('What is your name? ')
+gender = input('What is your gender? ')
+race = input('What race are you? (Human / Elf / Orc / Dwarf / Gnome / Sprite): ')
+description = input('Describe yourself in your own words: ')
+cool_line = '_____________________________________________'
+print('\n', cool_line)
+print('\t', name)
+print('\t', race, gender)
+print('\t', description)
+print(cool_line, '\n')
+
+
+
+
